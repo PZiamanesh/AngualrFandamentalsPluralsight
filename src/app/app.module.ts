10190713cec59e3cefccf7de9a86a379f81c6217
PZiamanesh <po.ziamanesh@gmail.com>
2023-12-12 21:31:15 +0330

    add catalog & header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CatalogComponent,
+    SiteHeaderComponent
+    ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
