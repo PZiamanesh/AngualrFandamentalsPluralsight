f02d9d085d683ee9b89bb851388df6c60f7643fe
PZiamanesh <po.ziamanesh@gmail.com>
2023-12-12 22:09:37 +0330

    add product model

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class CatalogComponent {
 
+  product : IProduct
+
+  constructor(){
+    this.product = {
+      id:2,
+      description: "a friendly robot for domestic use",
+      name: "Friendly Robot",
+      imageName: "head-friendly.png",
+      category: "Heads",
+      price: 945.0,
+      discount: 0.2,
+    }
+  }
+
+  getImageUrl(product:IProduct) : string {
+    return "/assets/images/robot-parts/" + product.imageName
+  }
 }
